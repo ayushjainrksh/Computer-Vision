5976e7ba1e98ab8f14c6c2e2af371f247dbaea2c
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-09-03 01:00:24 +0530

    Resized image saved to a file

--- imgscript.py
+++ imgscript.py
@@ -9,5 +9,6 @@ print(img.ndim)
 
 resized_img = cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
 cv2.imshow("galaxy",resized_img)
+cv2.imwrite("galaxy_resized.jpg",resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

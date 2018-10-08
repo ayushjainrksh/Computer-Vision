b04682c6e79fda949d3a37964e4eab64291e83a6
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-10-09 00:42:39 +0530

    Detected face resized to new image

--- Face Detection/faceDetector.py
+++ Face Detection/faceDetector.py
@@ -2,11 +2,11 @@ import cv2
 
 face_casecade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("photo.jpg")
+img = cv2.imread("news.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_casecade.detectMultiScale(gray_img,
-scaleFactor=1.05,
+scaleFactor=1.1,
 minNeighbors=5)
 
 for x, y, w, h in faces:
@@ -16,8 +16,9 @@ for x, y, w, h in faces:
 print(type(faces))
 print(faces)
 
+resized = cv2.resize(img, (int(img.shape[1]/3),int(img.shape[0]/3)))
 
-cv2.imshow("Gray", img)
+cv2.imshow("Gray", resized)
 
 cv2.waitKey(0)
 
